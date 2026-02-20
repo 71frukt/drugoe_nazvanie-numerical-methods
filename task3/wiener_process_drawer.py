dfaaf5292f9866af7d26b2795f5a48c6bc99766d
71frukt <71frukt@mail.ru>
2026-02-20 18:01:16 +0300

    graphic view of going beyond 3-sigma borders

--- task3/wiener_process_drawer.py
+++ task3/wiener_process_drawer.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def generate_wiener_process():
     T       = 1.0
     dt      = 0.0001
-    n_paths = 1000
+    n_paths = 5000
     
     n_steps = int(T / dt)
     
@@ -19,33 +19,51 @@ def generate_wiener_process():
     zeros = np.zeros((n_paths, 1))
     paths = np.hstack((zeros, cumulative_sum))
     
-    # --- Отрисовка ---
-    plt.figure(figsize=(12, 7))
-    
-    # Рисуем все траектории
-    # .T (транспонирование) нужно, так как plot рисует столбцы как отдельные линии
-    # alpha=0.1 делает линии полупрозрачными, чтобы было видно плотность распределения
-    plt.plot(timeline, paths.T, color='darkcyan', alpha=0.05, linewidth=0.5)
-    
-    # Дополнительно: Нарисуем границы 3 сигм (3 корня из t), чтобы проверить теорию
-    # Для Винеровского процесса ст. отклонение в момент t равно sqrt(t)
+    # Вычисление статистики для второго графика
     sigma_bound = 3 * np.sqrt(timeline)
-    plt.plot(timeline,  sigma_bound, color='red',   linestyle='--', linewidth=2, label='Граница $\pm 3\sqrt{t}$')
-    plt.plot(timeline, -sigma_bound, color='red',   linestyle='--', linewidth=2)
     
-    sigma_bound = 2 * np.sqrt(timeline)
-    plt.plot(timeline,  sigma_bound, color='green', linestyle='--', linewidth=2, label='Граница $\pm 2\sqrt{t}$')
-    plt.plot(timeline, -sigma_bound, color='green', linestyle='--', linewidth=2)
+    outside_condition = np.abs(paths[:, 1:]) > sigma_bound[1:]
+    outside_counts = np.sum(outside_condition, axis=0)
+    outside_percentage = (outside_counts / n_paths) * 100
+    
+    # Вычисление кумулятивного среднего для процента выходов
+    cumulative_avg_percentage = np.cumsum(outside_percentage) / np.arange(1, len(outside_percentage) + 1)
+    
+    # ----------------------------------- Отрисовка -------------------------------------------------------------
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(12, 12))
+    
+    # график 1 - траектории Винеровского процесса ---
+    ax1.plot(timeline, paths.T, color='darkcyan', alpha=0.05, linewidth=0.5)
+    
+    ax1.plot(timeline,  sigma_bound, color='red', linestyle='--', linewidth=2, label='Граница $\pm 3\sqrt{t}$')
+    ax1.plot(timeline, -sigma_bound, color='red', linestyle='--', linewidth=2)
 
-    plt.title(f'Винеровский процесс: {n_paths} траекторий\n$T={T}, \delta={dt}$', fontsize=14)
-    plt.xlabel('Время (t, сек)', fontsize=12)
-    plt.ylabel('Значение процесса ($B_t$)', fontsize=12)
-    plt.grid(True, which='both', linestyle='--', alpha=0.7)
-    plt.legend(loc='upper left')
+    ax1.set_title(f'Винеровский процесс: {n_paths} траекторий\n$T={T}, \delta={dt}$', fontsize=14)
+    ax1.set_xlabel('Время (t, сек)', fontsize=12)
+    ax1.set_ylabel('Значение процесса ($B_t$)', fontsize=12)
+    ax1.grid(True, which='both', linestyle='--', alpha=0.7)
+    ax1.legend(loc='upper left')
     
-    # Показать график
+    # график 2 - процент траекторий вне границ 3 сигм ---
+    ax2.plot(timeline[1:], outside_percentage, color='blue', alpha=0.3, label='Эмпирический процент')
+    
+    ax2.plot(timeline[1:], cumulative_avg_percentage, color='darkorange', linewidth=2, label='Среднее значение до точки x')
+    
+    ax2.axhline(y=0.27, color='red', linestyle='--', linewidth=2, label='Теоретический предел (0.27%)')
+    
+    ax2.set_title('Процент траекторий, вышедших за границы $\pm 3\sigma$', fontsize=14)
+    ax2.set_xlabel('Время (t, сек)', fontsize=12)
+    ax2.set_ylabel('Доля вышедших траекторий (%)', fontsize=12)
+    
+    ax2.set_xlim(0, T)
+    ax2.set_ylim(0, 0.8)
+    
+    ax2.grid(True, linestyle='--', alpha=0.7)
+    ax2.legend(loc='upper right')
+    
     plt.tight_layout()
     plt.show()
 
+# ----------------------------------- main ----------------------------------------------------------------------
 if __name__ == "__main__":
     generate_wiener_process()
