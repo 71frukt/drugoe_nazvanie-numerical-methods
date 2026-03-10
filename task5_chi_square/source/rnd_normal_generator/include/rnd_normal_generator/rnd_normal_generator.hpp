6a3e5bbc8b7a505b407f3af4bcc01b8755c90bbc
71frukt <71frukt@mail.ru>
2026-03-10 21:28:57 +0300

    add chi-square

---
+++ task5_chi_square/source/rnd_normal_generator/include/rnd_normal_generator/rnd_normal_generator.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <concepts>
+#include <random>
+namespace rnd_generators {
+
+class NormalGenerator
+{
+public:
+    NormalGenerator() = default;
+    
+    template <std::uniform_random_bit_generator generatorT, std::floating_point FloatingT = double>
+    FloatingT operator()(generatorT& generator)
+    {
+        if (has_spare_)
+        {
+            has_spare_ = false;
+            return spare_value_;
+        }
+
+        double point_x = 0;
+        double point_y = 0;
+        double radius  = 0;
+
+        do
+        {
+            point_x = 2.0 * ToCanonicalStrict(generator) - 1.0;
+            point_y = 2.0 * ToCanonicalStrict(generator) - 1.0;
+
+            radius  = point_x * point_x + point_y * point_y;
+
+        } while (radius >= 1.0 || radius == 0.0);
+
+        double phi_radius = std::sqrt(-2.0 * std::log(radius) / radius);
+
+        spare_value_ = point_y * phi_radius;
+        has_spare_   = true;
+
+        return         point_x * phi_radius;
+    }
+
+private:
+    bool   has_spare_   = false;
+    double spare_value_ = 0.0;
+
+public:
+    // makes generator value in (0, 1)
+    template <std::uniform_random_bit_generator generatorT, std::floating_point FloatingT = double>
+    static FloatingT ToCanonicalStrict(generatorT& generator)
+    {
+        constexpr FloatingT min_val = static_cast<FloatingT>(generatorT::min());
+        constexpr FloatingT max_val = static_cast<FloatingT>(generatorT::max());
+                  FloatingT res     = static_cast<FloatingT>(generator());
+        
+        return (res - min_val + 0.5) / (max_val - min_val + 1.0); 
+    }
+};
+
+
+} // namespace rnd_generators
